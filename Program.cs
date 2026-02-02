f024fa68dad02f5452573c1c6d0f08a96db55146
BaxtiyorCsharper <08baxti58.com>
2026-02-02 20:11:23 +0500

    Loyiha tayyor

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿decimal balls = 0;
+Console.WriteLine("\nIELTS-meterga xush kelibsiz!\n");
+string [] skills = {"speaking:","reading:","listening:","writing:"};
+Console.WriteLine("IELTS baholarini kiriting:\n");
+for (int i = 0; i < skills.Length; i++)
+{
+    Console.Write(skills[i]);
+    decimal ball = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine();
+    if (ball >= 0 && ball <= 9)
+    {
+        balls += ball;
+    }
+    else
+    {
+        Console.WriteLine("\nxato kiritildi:\n");
+    }
+}
+decimal result = balls / skills.Length;
+Console.WriteLine("Sizning EILTS natijangiz:"+result);
