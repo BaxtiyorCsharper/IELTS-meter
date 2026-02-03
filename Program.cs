472e85c0c0943bf851c3240e3da6e883feb01423
BaxtiyorCsharper <08baxti58.com>
2026-02-03 11:51:57 +0500

    Metod qo'shildi

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
-﻿decimal balls = 0;
-Console.WriteLine("\nIELTS-meterga xush kelibsiz!\n");
-string[] skills = { "speaking:", "reading:", "listening:", "writing:" };
-Console.WriteLine("IELTS baholarini kiriting:\n");
-for (int i = 0; i < skills.Length; i++)
+﻿static void PrintInfo()
 {
-  decimal ball = 0;
-  do
-  {
-    Console.Write($"{skills[i]} (0-9) ");
-    ball = Convert.ToDecimal(Console.ReadLine());
-    Console.WriteLine();
-  }
-  while (ball < 0 || ball > 9);
-
-  balls += ball;
+    Console.WriteLine("\nIELTS-meterga xush kelibsiz!\n");
 }
-decimal result = balls / skills.Length;
-Console.WriteLine("Sizning EILTS natijangiz:" + result);
+static decimal IeltsResult()
+{
+    decimal balls = 0;
+    string[] skills = { "speaking:", "reading:", "listening:", "writing:" };
+    Console.WriteLine("IELTS baholarini kiriting:\n");
+    for (int i = 0; i < skills.Length; i++)
+    {
+        decimal ball = 0;
+        do
+        {
+            Console.Write($"{skills[i]} (0-9) ");
+            ball = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine();
+        }
+        while (ball < 0 || ball > 9);
+
+        balls += ball;
+    }
+    decimal result = balls / skills.Length;
+    return result;
+}
+PrintInfo();
+decimal ieltsBall = IeltsResult();
+Console.WriteLine("Sizning EILTS natijangiz:" + ieltsBall);
+
