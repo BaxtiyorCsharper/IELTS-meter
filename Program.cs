3f65bde23c6a2b6339d681ae3a344d8e7ee526ff
BaxtiyorCsharper <08baxti58.com>
2026-02-03 08:56:57 +0500

    Validatsiya to'g'irlandi

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
 ﻿decimal balls = 0;
 Console.WriteLine("\nIELTS-meterga xush kelibsiz!\n");
-string [] skills = {"speaking:","reading:","listening:","writing:"};
+string[] skills = { "speaking:", "reading:", "listening:", "writing:" };
 Console.WriteLine("IELTS baholarini kiriting:\n");
 for (int i = 0; i < skills.Length; i++)
 {
-    Console.Write(skills[i]);
-    decimal ball = Convert.ToDecimal(Console.ReadLine());
+  decimal ball = 0;
+  do
+  {
+    Console.Write($"{skills[i]} (0-9) ");
+    ball = Convert.ToDecimal(Console.ReadLine());
     Console.WriteLine();
-    if (ball >= 0 && ball <= 9)
-    {
-        balls += ball;
-    }
-    else
-    {
-        Console.WriteLine("\nxato kiritildi:\n");
-    }
+  }
+  while (ball < 0 || ball > 9);
+
+  balls += ball;
 }
 decimal result = balls / skills.Length;
-Console.WriteLine("Sizning EILTS natijangiz:"+result);
+Console.WriteLine("Sizning EILTS natijangiz:" + result);
